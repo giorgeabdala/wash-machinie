a3393fccccc58fcb052293fa9a572fb52e176053
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2022-09-04 21:49:41 +0000

    updated lambda/config/monkeyUrl.js.

--- lambda/config/monkeyUrl.js
+++ lambda/config/monkeyUrl.js
@@ -1,9 +1,9 @@
 
 const monkeyUrl = {
-    OnfOff: 'SECRET_URL',
-    jump: 'SECRET_URL',
-    Drying: 'SECRET_URL',
-    Rising: 'SECRET_URL',
+    OnfOff: 'https://api.voicemonkey.io/trigger?access_token=4e8c93f35c2de1e32d6454dc811eae75&secret_token=87a4b40a6b81ed6c80c54e333f1047b3&monkey=ligarmaquinamonkey&announcement=Hello%20monkey',
+    jump: 'https://api.voicemonkey.io/trigger?access_token=4e8c93f35c2de1e32d6454dc811eae75&secret_token=87a4b40a6b81ed6c80c54e333f1047b3&monkey=pularmaquinamonkey&announcement=Hello%20monkey',
+    Drying: 'https://api.voicemonkey.io/trigger?access_token=4e8c93f35c2de1e32d6454dc811eae75&secret_token=87a4b40a6b81ed6c80c54e333f1047b3&monkey=turbosecagemmaquinamonkey&announcement=Hello%20monkey',
+    Rising: 'https://api.voicemonkey.io/trigger?access_token=4e8c93f35c2de1e32d6454dc811eae75&secret_token=87a4b40a6b81ed6c80c54e333f1047b3&monkey=duploenxaguemaquinamonkey&announcement=Hello%20monkey',
 }
 
 module.exports = monkeyUrl;
